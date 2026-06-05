394ab94fcc1b937c2df3b959c8d0549ac6d7489a
Fady Malek <fadymalk1234@gmail.com>
2026-06-05 12:53:41 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import FileComponent from "./components/FileComponent";
+
+function App() {
+  return (
+    <div>
+      <FileComponent filename={"index.tsx"} />
+      <FileComponent filename={"index.html"} />
+    </div>
+  );
+}
+
+export default App;
