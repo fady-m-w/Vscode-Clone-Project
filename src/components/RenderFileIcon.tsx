cf834d91a0db53c409c2e1796848d37624919c29
Fady Malek <fadymalk1234@gmail.com>
2026-06-10 10:43:13 +0300

    Refactoring Render Icon Component

--- src/components/RenderFileIcon.tsx
+++ src/components/RenderFileIcon.tsx
@@ -1,3 +1,4 @@
+import { extensionIconPaths } from "../constant";
 import IconImg from "./IconImg";
 import FileIcon from "./SVG/File";
 
@@ -11,39 +12,15 @@ const RenderFileIcon = ({ filename, isFolder, isOpen }: Iprops) => {
   const extension = filename.split(".").pop();
   console.log(extension);
 
-  //   ** FILES
-  if (extension === "tsx") return <IconImg src="/icons/react_ts.svg" />;
-  if (extension === "js") return <IconImg src="/icons/javascript.svg" />;
-  if (extension === "jsx") return <IconImg src="/icons/react.svg" />;
-  if (extension === "html") return <IconImg src="/icons/html.svg" />;
+  if (extension && Object.hasOwn(extensionIconPaths, extension)) {
+    const iconPath = isFolder
+      ? isOpen
+        ? `${extensionIconPaths[extension]}-open.svg`
+        : `${extensionIconPaths[extension]}.svg`
+      : `${extensionIconPaths[extension]}.svg`;
 
-  //   ** FOLDERS
-  if (extension === "node_modules" && isFolder)
-    return isOpen ? (
-      <IconImg src="/icons/folder-node-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-node.svg" />
-    );
-
-  if (extension === "public" && isFolder)
-    return isOpen ? (
-      <IconImg src="/icons/folder-public-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-public.svg" />
-    );
-
-  if (extension === "src" && isFolder)
-    return isOpen ? (
-      <IconImg src="/icons/folder-src-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-src.svg" />
-    );
-  if (extension === "components" && isFolder)
-    return isOpen ? (
-      <IconImg src="/icons/folder-components-open.svg" />
-    ) : (
-      <IconImg src="/icons/folder-components.svg" />
-    );
+    return <IconImg src={iconPath} />;
+  }
 
   if (isFolder && isOpen)
     return <IconImg src="/icons/folder-default-open.svg" />;
---
+++ src/constant/index.ts
@@ -0,0 +1,13 @@
+export const extensionIconPaths: Record<string, string> = {
+  //   ** FILES
+  tsx: "/icons/react_ts",
+  js: "/icons/javascript",
+  jsx: "/icons/react",
+  html: "/icons/html",
+
+  //   ** FOLDERS
+  node_modules: "/icons/folder-node",
+  public: "/icons/folder-public",
+  src: "/icons/folder-src",
+  components: "/icons/folder-components",
+};
